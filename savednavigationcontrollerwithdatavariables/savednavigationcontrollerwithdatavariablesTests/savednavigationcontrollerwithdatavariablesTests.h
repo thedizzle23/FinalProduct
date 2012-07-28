734e4b09f0e8e0580dec2768aa0a6705b41e2642
Robert DeNicola <thedizzle23@gmail.com>
2012-07-28 15:50:22 -0400

    complete commit 11a

---
+++ savednavigationcontrollerwithdatavariables/savednavigationcontrollerwithdatavariablesTests/savednavigationcontrollerwithdatavariablesTests.h
@@ -0,0 +1,13 @@
+//
+//  savednavigationcontrollerwithdatavariablesTests.h
+//  savednavigationcontrollerwithdatavariablesTests
+//
+//  Created by Rob DeNicola on 7/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface savednavigationcontrollerwithdatavariablesTests : SenTestCase
+
+@end
---
+++ savednavigationcontrollerwithdatavariables/savednavigationcontrollerwithdatavariablesTests/savednavigationcontrollerwithdatavariablesTests.m
@@ -0,0 +1,32 @@
+//
+//  savednavigationcontrollerwithdatavariablesTests.m
+//  savednavigationcontrollerwithdatavariablesTests
+//
+//  Created by Rob DeNicola on 7/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "savednavigationcontrollerwithdatavariablesTests.h"
+
+@implementation savednavigationcontrollerwithdatavariablesTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in savednavigationcontrollerwithdatavariablesTests");
+}
+
+@end
